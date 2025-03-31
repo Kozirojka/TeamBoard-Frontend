677b50ae36728e9f7505f9d51ec3a3dbf04337bd
Stalker <vihovaneckostik@gmil.com>
2025-03-31 18:19:21 +0300

    some chagnes

--- TeamBoard-Client/src/App.jsx
+++ TeamBoard-Client/src/App.jsx
@@ -1,18 +1,26 @@
 import './App.css'
-
 function App() {
 
-  const handleLoginWithGoogle = () => {
-    window.location.href = 'https://localhost:5187/api/account/login/google?returnUrl=http://localhost:5173';
-  }
+  // const handleLoginWithGoogle = () => {
+  //   window.location.href = 'https://localhost:5187/api/account/login/google?returnUrl=http://localhost:5173';
+  // }
 
-  return (
-    <>
-      <button onClick={handleLoginWithGoogle}>
-        Sign in with Google
-      </button>
-    </>
+  // return (
+  //   <>
+  //     <button onClick={handleLoginWithGoogle}>
+  //       Sign in with Google
+  //     </button>
+  //   </>
+  // )
+
+  return(
+    <div className="App">
+      <header className="App-header">
+        <h1>React App</h1>
+      </header>
+    </div>
   )
+
 }
 
 export default App
